5146c4eb20a95fb18d56675176f861b4c57136e1
Jean-Jacques Serpoul <serpoul@gmail.com>
2015-08-24 20:10:56 +0200

    Réécriture mangosteam_test

--- mangosteam_test.go
+++ mangosteam_test.go
@@ -2,7 +2,7 @@ package mangosteam
 
 import "testing"
 
-func TestString(t *testing.T) {
+func TestSteamIDString(t *testing.T) {
 	steamID := SteamID(1234567890123)
 	expectedStrSteamID := "1234567890123"
 	gotStrSteamID := steamID.String()
@@ -11,6 +11,95 @@ func TestString(t *testing.T) {
 		t.Errorf(
 			"steamID %v .String(), expected %v, got %v",
 			steamID, expectedStrSteamID, gotStrSteamID,
+		)
+	}
+}
+
+func TestContextIdString(t *testing.T) {
+	contextID := ContextID(1234567890123)
+	expectedContextID := "1234567890123"
+	gotExpectedContextID := contextID.String()
+
+	if gotExpectedContextID != expectedContextID {
+		t.Errorf(
+			"contextID %v , expected %v, got %v",
+			contextID, expectedContextID, gotExpectedContextID,
+		)
+	}
+}
+
+func TestApIdString(t *testing.T) {
+	appID := AppID(123456789)
+	expectedAppID := "123456789"
+	gotExpectedAppID := appID.String()
+
+	if gotExpectedAppID != expectedAppID {
+		t.Errorf(
+			"appID %v, expected %v, got %v",
+			appID, expectedAppID, gotExpectedAppID,
+		)
+	}
+}
+
+func TestGetAccountIdString(t *testing.T) {
+	steamID := SteamID(1234567890123)
+	expectedAccountID := "1912276171"
+	GotExpectedAccountID := steamID.GetAccountID()
+
+	if GotExpectedAccountID != expectedAccountID {
+		t.Errorf(
+			"Expected %v, got %v",
+			expectedAccountID, GotExpectedAccountID,
+		)
+	}
+}
+
+func TestGetSteamIDFromString(t *testing.T) {
+
+	expectedSteamIDFromString := SteamID(1234567890123)
+	GotSteamIDFromString, _ := GetSteamIDFromString("1234567890123")
+
+	if GotSteamIDFromString != expectedSteamIDFromString {
+		t.Errorf(
+			"Expected %v, got %v",
+			expectedSteamIDFromString, GotSteamIDFromString,
+		)
+	}
+}
+
+func TestGetAppIDFromString(t *testing.T) {
+	expectedGetAppIDFromString := AppID(123456789)
+	GotGetAppIDFromString, _ := GetAppIDFromString("123456789")
+
+	if GotGetAppIDFromString != expectedGetAppIDFromString {
+		t.Errorf(
+			"Expected %v, got %v",
+			expectedGetAppIDFromString, GotGetAppIDFromString,
+		)
+	}
+}
+
+func TestErrorGetSteamIDFromString(t *testing.T) {
+
+	expectedSteamIDFromString := SteamID(12345678901236)
+	GotSteamIDFromString, err := GetSteamIDFromString("1234567890123A")
+
+	if err != nil {
+		t.Errorf(
+			"Should be an error, Expected %v, got %v",
+			expectedSteamIDFromString, GotSteamIDFromString,
+		)
+	}
+}
+
+func TestErrorGetAppIDFromString(t *testing.T) {
+	expectedGetAppIDFromString := AppID(123456755)
+	GotGetAppIDFromString, err := GetAppIDFromString("12345679A")
+
+	if err != nil {
+		t.Errorf(
+			"Should be an error, Expected %v, got %v",
+			expectedGetAppIDFromString, GotGetAppIDFromString,
 		)
 	}
 }
