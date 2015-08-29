ed3668674de5c7f428563758727da1c305a5021e
Jean-Jacques Serpoul <serpoul@gmail.com>
2015-08-30 01:32:18 +0200

    Test login.go 100%

--- auth/login_test.go
+++ auth/login_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestDoLogin(t *testing.T) {
@@ -36,7 +37,186 @@ func TestDoLogin(t *testing.T) {
 	)
 
 	if err == nil {
-		t.Errorf("Dologin returns no error when login is not successful")
+		t.Errorf("Dologin returns no error when login failed")
+	}
+
+	return
+}
+func TestDoOKLogin(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, getMockOKLoginDologin())
+	}))
+	defer ts.Close()
+
+	client := http.Client{}
+	username := "mangosteam"
+	encryptedPassword := "123"
+	rsatimestamp := "123"
+	emailauthKeyedIn := ""
+	captchaGID := ""
+	captchaKeyedIn := ""
+
+	err := DoLogin(
+		ts.URL,
+		&client,
+		username,
+		encryptedPassword,
+		rsatimestamp,
+		emailauthKeyedIn,
+		captchaGID,
+		captchaKeyedIn,
+	)
+
+	if err != nil {
+		t.Errorf("Dologin returns error when login successfull")
+	}
+
+	return
+}
+
+func TestHttpNotOKLogin(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	defer ts.Close()
+
+	client := http.Client{}
+	username := "mangosteam"
+	encryptedPassword := "123"
+	rsatimestamp := "123"
+	emailauthKeyedIn := ""
+	captchaGID := ""
+	captchaKeyedIn := ""
+
+	err := DoLogin(
+		ts.URL,
+		&client,
+		username,
+		encryptedPassword,
+		rsatimestamp,
+		emailauthKeyedIn,
+		captchaGID,
+		captchaKeyedIn,
+	)
+
+	if err == nil {
+		t.Errorf("Dologin returns no error when http status is not statusOK")
+	}
+
+	return
+}
+
+func TestKODoLoginForm(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		// An error is returned if caused by client policy (such as CheckRedirect),
+		// or if there was an HTTP protocol error. A non-2xx response doesn't cause an error.
+		time.Sleep(200 * time.Millisecond)
+	}))
+	defer ts.Close()
+	// Change timeOut to make an HTTP protocol error timeout
+	var myTransport http.RoundTripper = &http.Transport{
+		ResponseHeaderTimeout: time.Millisecond * 100,
+	}
+
+	client := http.Client{Transport: myTransport}
+
+	username := "mangosteam"
+	encryptedPassword := "123"
+	rsatimestamp := "123"
+	emailauthKeyedIn := ""
+	captchaGID := ""
+	captchaKeyedIn := ""
+	err := DoLogin(
+		ts.URL,
+		&client,
+		username,
+		encryptedPassword,
+		rsatimestamp,
+		emailauthKeyedIn,
+		captchaGID,
+		captchaKeyedIn,
+	)
+
+	if err == nil {
+		t.Errorf("Dologin returns no error when DoLogin Form failed")
+	}
+
+	return
+}
+
+func TestEmailauthNeeded(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, getMockEmailauthNeeded())
+	}))
+	defer ts.Close()
+
+	client := http.Client{}
+	username := "mangosteam"
+	encryptedPassword := "123"
+	rsatimestamp := "123"
+	emailauthKeyedIn := ""
+	captchaGID := ""
+	captchaKeyedIn := ""
+
+	err := DoLogin(
+		ts.URL,
+		&client,
+		username,
+		encryptedPassword,
+		rsatimestamp,
+		emailauthKeyedIn,
+		captchaGID,
+		captchaKeyedIn,
+	)
+
+	if err == nil {
+		t.Errorf("AuthLogin should have Email Needed")
+	}
+
+	return
+}
+
+func TestRespBody(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, getMockRespBody())
+	}))
+	defer ts.Close()
+
+	client := http.Client{}
+	username := "mangosteam"
+	encryptedPassword := "123"
+	rsatimestamp := "123"
+	emailauthKeyedIn := ""
+	captchaGID := ""
+	captchaKeyedIn := ""
+
+	err := DoLogin(
+		ts.URL,
+		&client,
+		username,
+		encryptedPassword,
+		rsatimestamp,
+		emailauthKeyedIn,
+		captchaGID,
+		captchaKeyedIn,
+	)
+
+	if err == nil {
+		t.Errorf("loginBody returns no error")
 	}
 
 	return
