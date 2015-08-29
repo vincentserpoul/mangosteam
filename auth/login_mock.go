46b933509b4abba03f6526d0bd04b8a0bcc579fa
Jean-Jacques Serpoul <serpoul@gmail.com>
2015-08-30 01:34:28 +0200

    Ajout pour test bad respBoby

--- auth/login_mock.go
+++ auth/login_mock.go
@@ -12,3 +12,42 @@ func getMockKOLoginDologin() string {
 		}
 		`
 }
+func getMockOKLoginDologin() string {
+
+	return `
+		{
+			"success":true,
+			"requires_twofactor":false,
+			"login_complete":true,
+			"transfer_url":"https:\/\/steamcommunity.com\/login\/transfer",
+			"transfer_parameters":
+			{
+				"steamid":"75894854842678978",
+				"token":"2F55FFBBB857485858CC5785966AAA4585585518",
+				"auth":"4AF5848554B8547855C8574580852828",
+				"remember_login":false,
+				"token_secure":"85440F8545A46454D4854848422484248444BFFB"}
+			}
+		}
+		`
+}
+
+func getMockEmailauthNeeded() string {
+
+	return `
+		{
+		  "success": false,
+		  "emailauth_needed": true
+		}
+		`
+}
+
+func getMockRespBody() string {
+
+	return `
+		{
+		  "success": false,,
+		  "emailauth_needed: true
+		}
+		`
+}
