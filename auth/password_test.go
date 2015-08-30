7f4f025e1444f6bfbf2dc7225dcff7b860249145
Jean-Jacques Serpoul <serpoul@gmail.com>
2015-08-30 20:52:34 +0200

    Test password - pas de besoin du ReadTimeout, WriteTimeOut suffit

--- auth/password_test.go
+++ auth/password_test.go
@@ -213,7 +213,6 @@ func TestKODoLoginPostForm(t *testing.T) {
 		// or if there was an HTTP protocol error. A non-2xx response doesn't cause an error.
 		time.Sleep(200 * time.Millisecond)
 	}))
-	ts.Config.ReadTimeout = 20 * time.Millisecond
 	ts.Config.WriteTimeout = 20 * time.Millisecond
 	defer ts.Close()
 	_, err := GetRSAKey(ts.URL, username)
