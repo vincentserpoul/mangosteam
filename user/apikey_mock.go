2d3487550c05a4127005886553a5c265954dea6f
vserpoul <vincent@serpoul.com>
2015-09-01 18:47:07 +0800

    changing function name

--- user/apikey_mock.go
+++ user/apikey_mock.go
@@ -1,6 +1,6 @@
 package steamuser
 
-func getMockOKLoginGetrsakey() string {
+func getMockExistingAPIKeyPage() string {
 	return `
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
         <html>
@@ -8,21 +8,21 @@ func getMockOKLoginGetrsakey() string {
         <title>Steam</title>
         </head>
         <body>
-        <!-- main body -->
-        <div id="BG_bottom">
-            <div id="bodyContents_ex">
+            <!-- main body -->
+            <div id="BG_bottom">
+                <div id="bodyContents_ex">
 
-                    <h2>Your Steam Web API Key</h2>
-                    <p>Key: 1234567890123456789012</p>
-                    <p>Domain name: motus.com</p>
-                    <form class="smallForm" id="editForm" name="editForm" method="POST" action="http://fakeurl.com/dev/revokekey" onSubmit="return verifyRevoke()">
-                    <input type="submit" name="Revoke" value="">
-                    <input type="hidden" name="sessionid" value="fakesession">
-                    </form>
+                        <h2>Your Steam Web API Key</h2>
+                        <p>Key: 1234567890123456789012</p>
+                        <p>Domain name: motus.com</p>
+                        <form class="smallForm" id="editForm" name="editForm" method="POST" action="http://fakeurl.com/dev/revokekey" onSubmit="return verifyRevoke()">
+                        <input type="submit" name="Revoke" value="">
+                        <input type="hidden" name="sessionid" value="fakesession">
+                        </form>
 
 
+                </div>
             </div>
-        </div>
         </body>
         </html>
     `
